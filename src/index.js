3ddc345d9afbd655c22934f9364bf408c50cb96d
angrycola <youagain@yandex.com>
2017-11-03 02:24:37 +0300

    Redux init

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './styles/index.css';
-import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
+import './styles/index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import App from './components/App';
+import { Provider } from 'react-redux';
+import { store } from './store';
+
+ReactDOM.render(
+	<Provider store={ store }>
+		<App />
+	</Provider>, document.getElementById('root')
+);
+
 registerServiceWorker();
---
+++ src/reducers/reducer.js
@@ -0,0 +1,6 @@
+// import C from '../constants';
+export default function(state={}, action) {
+	switch (action.type) {
+	default: return state;
+	}
+}
